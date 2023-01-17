55df6e8f6dccc24f4182ea22b8738a3f4b78779b
ahmedev192 <ahmedev192@gmail.com>
2023-01-17 21:51:16 +0200

    Till Sec7

---
+++ BulkyBookWeb/Program.cs
@@ -0,0 +1,44 @@
+using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace BulkyBookWeb
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = WebApplication.CreateBuilder(args);
+
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+			builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+				builder.Configuration.GetConnectionString("DefaultConnection")
+				));
+
+   builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+			builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+			builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+			var app = builder.Build();
+
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+			}
+			app.UseStaticFiles();
+
+			app.UseRouting();
+
+			app.UseAuthorization();
+
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{area=Customer}/{controller=Home}/{action=Index}/{id?}");
+
+			app.Run();
+		}
+	}
+}
